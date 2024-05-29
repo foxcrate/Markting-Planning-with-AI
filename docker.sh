95a9b554a3b8aa8e248dec949f421a574a8a2ebd
modeht <modeht@gmail.com>
2024-05-29 17:39:43 +0300

    feat: docker file upload

--- docker.sh
+++ docker.sh
@@ -3,4 +3,4 @@ docker stop crespo-staging;
 docker rm crespo-staging;
 docker image rm crespo/staging;
 docker build . --tag crespo/staging;
-docker run -d -p 127.0.0.1:3011:3000 --restart unless-stopped --name crespo-staging crespo/staging;
+docker run -d -p 127.0.0.1:3011:3000 -v ./src/public/:/app/public --restart unless-stopped --name crespo-staging crespo/staging;
