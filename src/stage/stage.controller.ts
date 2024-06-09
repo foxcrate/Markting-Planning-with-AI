ac288c4ca9962f7e84495685b5b87cac4b71e6fc
foxcrate <a.fawzy@wecodeforyou.io>
2024-06-09 23:16:50 +0300

    add tactic to stage

--- src/stage/stage.controller.ts
+++ src/stage/stage.controller.ts
@@ -1,10 +1,11 @@
-import { Body, Controller, Get, Param, Put, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Put, UseGuards } from '@nestjs/common';
 import { AuthGuard } from 'src/gurads/auth.guard';
 import { StageService } from './stage.service';
 import { UserId } from 'src/decorators/user-id.decorator';
 import { FunnelIdAndStageIdDto } from './dtos/funnelId-and-stageId.dto';
 import { FunnelService } from 'src/funnel/funnel.service';
 import { updateStageTacticsOrderDto } from './dtos/update-stage-tactics-order.dto';
+import { TacticIdDto } from 'src/tactic/dtos/tactic-id.dto';
 
 @Controller({ path: 'funnel/:funnelId/stage', version: '1' })
 export class StageController {
@@ -37,6 +38,20 @@ export class StageController {
       theFunnel.userId,
       userId,
       body.tactics,
+    );
+  }
+
+  @Post(':stageId/add-tactic')
+  @UseGuards(AuthGuard)
+  async addTacticToStage(
+    @Param() params: FunnelIdAndStageIdDto,
+    @Body() body: TacticIdDto,
+    @UserId() userId: number,
+  ) {
+    return await this.tacticService.addTacticToStage(
+      body.tacticId,
+      body.stageId,
+      userId,
     );
   }
 }
