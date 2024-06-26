1ffbf59d991ff9e590fece943a3bafcb980499c6
foxcrate <ahmedmustafa.pro19@gmail.com>
2024-06-26 23:25:36 +0300

    checkbox tactics

--- src/main.ts
+++ src/main.ts
@@ -60,9 +60,15 @@ async function bootstrap() {
 
   var serviceAccount = process.env.FIREBASE_ADMIN_AUTH_PATH;
 
-  admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
+  // admin.initializeApp({
+  //   credential: admin.credential.cert(serviceAccount),
+  // });
+
+  const firebaseAdminApp = admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount as admin.ServiceAccount),
   });
+
+  console.log(firebaseAdminApp);
 
   await app.listen(process.env.APP_PORT || 3000, '0.0.0.0');
 
